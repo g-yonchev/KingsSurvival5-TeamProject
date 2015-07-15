cdb0e002d5d34eebf77e951becd0dc038bea0590
dushka-dragoeva <d.dragoeva@abv.bg>
2015-07-15 06:56:44 +0300

    Added some constants

---
+++ King-Survival-5/Commons/Constants.cs
@@ -0,0 +1,24 @@
+﻿namespace King.Commons
+{
+    using System;
+   public static class Constants
+    {
+        public const string FINISH_GAME_MESSAGE = "Game is finished!";
+        public const string INVALID_COMMAND_MESSAGE = "Invalid command name!";
+        public const string ERROR_MESSAGE = "Sorry, there are some errors, but I can't tell you anything! You broked my program!";
+        public const string KING_TURN_MESSAGE = "Please enter king's turn: ";
+        public const string EMPTY_STRING_MESSAGE = "Empty command!";
+        public const string PAWN_TURN_MASSAGE = "Please enter pawn's turn: ";
+        public const string END_GAME_MESSAGE = "End!";
+        public const string KING_VICTORY_MESSAGE = "King wins in {0} moves!";
+        public const string WRONG_DIRECTION_MESSAGE = "You can't go in this direction! ";
+        public const string KING_LOST_MESSAGE = "King losesin {0} moves!";
+        public const string GOODBYE_MESSAGE = "\nThank you for using this game!\n\n";
+
+
+
+
+
+
+    }
+}
