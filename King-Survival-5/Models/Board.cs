8e5bc86e5173e7d875541d8799c872a1fc0812c9
g-yonchev <georgi.d.yonchev@gmail.com>
2015-09-26 15:20:05 +0300

    Created class Board.cs which is Singleton

---
+++ King-Survival-5/Models/Board.cs
@@ -0,0 +1,39 @@
+﻿namespace King
+{
+    public class Board
+    {
+        private static volatile Board instance;
+        
+        private static readonly object syncLock = new object();
+        
+        private Board(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public static Board Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncLock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Board(8, 8);
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+    }
+}
