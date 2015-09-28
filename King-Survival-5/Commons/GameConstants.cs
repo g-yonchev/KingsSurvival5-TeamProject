37a96d167dba2141e8384efc0fe41d7e290f0e14
dushka-dragoeva <d.dragoeva@abv.bg>
2015-09-28 14:06:00 +0300

    Added new GameConstants.cs for all other magic numbers and stings

---
+++ King-Survival-5/Commons/GameConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace King.Commons
+{
+    class GameConstants
+    {
+        internal const string RegexKingPattern = @"(K(U|D)(L|R)";
+        internal const string RegexPawnPattern = @"(A|B|C|D)(U|D)(L|R)";
+    }
+}
