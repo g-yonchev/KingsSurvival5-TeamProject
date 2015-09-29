356413e7cd1c44f94e4fcf0909fe4e0890a8a940
dushka-dragoeva <d.dragoeva@abv.bg>
2015-09-29 16:50:57 +0300

    added Validator class

---
+++ King-Survival-5/Commons/Validator.cs
@@ -0,0 +1,46 @@
+﻿namespace KingSurvival.Commons
+{
+    using Commons;
+    using global::Commons;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    class Validator
+    {
+        internal static bool IsValidPawnMove(string input)
+        {
+            string validInput = input.ToUpper();
+            var regex = new Regex(GameConstants.RegexPawnPattern);
+
+            if (regex.IsMatch(validInput))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+       internal static bool IsValidKingMove(string input)
+        {
+            string validInput = input.ToUpper();
+            var regex = new Regex(GameConstants.RegexKingPattern);
+
+            if (regex.IsMatch(validInput))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
