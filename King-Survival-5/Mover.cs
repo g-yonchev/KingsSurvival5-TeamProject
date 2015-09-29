05465a740f8ba44a6d8c8d86d76e35c255a7f21a
BrunoHautenfaust <t1m2n3t4@yahoo.com>
2015-09-29 13:04:12 +0300

    even more separation

---
+++ King-Survival-5/Mover.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using System;
+
+
+namespace KingSurvival
+{
+    public static class Mover
+    {
+        public static void MoveFigure(IPosition currentCoordinates, IPosition newCoordinates)
+        {
+            char currentPos = BaseGame.GetField[currentCoordinates.Row, currentCoordinates.Col];
+            BaseGame.GetField[currentCoordinates.Row, currentCoordinates.Col] = ' ';
+            BaseGame.GetField[newCoordinates.Row, newCoordinates.Col] = currentPos;
+        }
+    }
+}
