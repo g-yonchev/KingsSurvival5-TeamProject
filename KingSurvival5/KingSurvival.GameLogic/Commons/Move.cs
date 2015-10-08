3a7782d9a808badfdae6c00d2bbae423a51960e4
DennyGD <denny.dimitrova@abv.bg>
2015-10-09 00:34:21 +0300

    Introduced new class Move

---
+++ KingSurvival5/KingSurvival.GameLogic/Commons/Move.cs
@@ -0,0 +1,17 @@
+﻿namespace KingSurvival.GameLogic.Commons
+{
+    using KingSurvival.GameLogic.Contracts;
+
+    public class Move
+    {
+        public Move(IPosition currentPosition, IPosition nextPosition)
+        {
+            this.CurrentPosition = currentPosition;
+            this.NextPosition = nextPosition;
+        }
+
+        public IPosition CurrentPosition { get; set; }
+
+        public IPosition NextPosition { get; set; }
+    }
+}
