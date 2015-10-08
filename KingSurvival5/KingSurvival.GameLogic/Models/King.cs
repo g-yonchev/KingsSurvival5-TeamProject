331aaf9a3c828b098b5d72acf403706451521e4d
DennyGD <denny.dimitrova@abv.bg>
2015-10-08 15:01:44 +0300

    Renamed classes Pawn and Figure

--- KingSurvival5/KingSurvival.GameLogic/Models/King.cs
+++ KingSurvival5/KingSurvival.GameLogic/Models/King.cs
@@ -2,16 +2,22 @@
 {
     using KingSurvival.GameLogic.Contracts;
 
-    public class PawnFigure : Figure, IFigure
+    public class King : Figure, IFigure
     {
-        public PawnFigure(Position position)
+        private const int InitialMovesCount = 0;
+
+        public int MovesCount { get; private set; }
+
+        public King(Position position)
             : base(position)
         {
+            this.MovesCount = InitialMovesCount;
         }
 
         public override void Move(Position position)
         {
             this.Position = position;
+            this.MovesCount++;
         }
     }
 }
