64802c667df642b0df20de7ed59f0f66f2ffe3b0
g-yonchev <georgi.d.yonchev@gmail.com>
2015-10-08 12:47:31 +0300

    Fixed something in FigurePrototype.cs

--- KingSurvival5/KingSurvival.GameLogic/Commons/FigurePrototype.cs
+++ KingSurvival5/KingSurvival.GameLogic/Commons/FigurePrototype.cs
@@ -1,4 +1,9 @@
-﻿namespace KingSurvival.GameLogic.Commons
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KingSurvival.GameLogic.Commons
 {
     public abstract class FigurePrototype
     {
