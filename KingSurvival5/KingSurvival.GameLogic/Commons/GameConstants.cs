1e7606f43982956def22653a05ee60f8f992f5fc
dushka-dragoeva <d.dragoeva@abv.bg>
2015-10-08 13:39:06 +0300

    Added some constants for magic numbers

--- KingSurvival5/KingSurvival.GameLogic/Commons/GameConstants.cs
+++ KingSurvival5/KingSurvival.GameLogic/Commons/GameConstants.cs
@@ -10,5 +10,25 @@
         public const int MinNumberOfRows = 0;
         public const int MinNumberOfCols = 0;
 
+        public const int BoardHeight = 8;
+        public const int BoardWidth = 17;
+
+        public const int FirstPawnInitialRow = 0;
+        public const int FirstPawnInitialCol = 0;
+
+        public const int SecondPawnInitialRow = 0;
+        public const int SecondPawnInitialCol = 2;
+
+        public const int ThirdPawnInitialRow = 0;
+        public const int ThirdPawnInitialCol = 4;
+
+        public const int FourthPawnInitialRow = 0;
+        public const int FourthPawnInitialCol = 6;
+
+        public const int KingInitialRow = 7;
+        public const int KingInitialCol = 3;
+
+
+
     }
 }
