37d76439f8d4287283ad96fddad049fd95740745
DennyGD <denny.dimitrova@abv.bg>
2015-10-09 01:31:39 +0300

    Implemented PawnMovementStrategy

---
+++ KingSurvival5/KingSurvival.GameLogic/MovementStrategies/PawnMovementStrategy.cs
@@ -0,0 +1,27 @@
+﻿namespace KingSurvival.GameLogic.MovementStrategies
+{
+    using KingSurvival.GameLogic.Commons;
+
+    public class PawnMovementStrategy : IMovementStrategy
+    {
+        public bool CanMove(Movement movement)
+        {
+            int rowDistance = movement.CurrentPosition.Row - movement.NextPosition.Row;
+            int colDistance = movement.CurrentPosition.Col - movement.NextPosition.Col;
+
+            bool movementIsDownLeft = (rowDistance == -1) && (colDistance == 1);
+            if (movementIsDownLeft)
+            {
+                return true;
+            }
+
+            bool movementIsDownRight = (rowDistance == 1) && (colDistance == -1);
+            if (movementIsDownRight)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
