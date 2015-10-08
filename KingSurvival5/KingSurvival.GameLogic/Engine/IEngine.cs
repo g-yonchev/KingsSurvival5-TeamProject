b16e877ba6d010e1dd32fb578bc8dee8355e71be
g-yonchev <georgi.d.yonchev@gmail.com>
2015-10-08 15:39:15 +0300

    Implemented Engine.cs

---
+++ KingSurvival5/KingSurvival.GameLogic/Engine/IEngine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingSurvival.GameLogic.Engine
+{
+    public interface IEngine
+    {
+        void Initialize();
+
+        void Start();
+    }
+}
