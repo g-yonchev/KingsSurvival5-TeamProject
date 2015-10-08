083e00c807bfaa79982ecbc44ae7100b163efae3
dushka-dragoeva <d.dragoeva@abv.bg>
2015-10-08 16:39:45 +0300

    Added Interface IBoard.cs

---
+++ KingSurvival5/KingSurvival.GameLogic/Contracts/IBoard.cs
@@ -0,0 +1,17 @@
+﻿namespace KingSurvival.GameLogic.Contracts
+{
+    using KingSurvival.GameLogic.Models;
+
+    public interface IBoard
+    {
+        int TotalRows { get; }
+
+        int TotalCols { get; }
+
+        void AddFigure(IFigure figure, Position position);
+
+        IFigure GetFigureAtPosition(Position position);
+
+        void MoveFigureAtPosition(IFigure figure, Position from, Position to);
+    }
+}
