f7f32a52079a0216b181cc5aec28332d5d856f55
DennyGD <denny.dimitrova@abv.bg>
2015-10-11 14:02:25 +0300

    Changed Board specs

--- KingSurvival5/KingSurvival.GameLogic/Models/Board.cs
+++ KingSurvival5/KingSurvival.GameLogic/Models/Board.cs
@@ -6,19 +6,33 @@
 
     public class Board : IBoard
     {
+        private char[,] field;
+
         public Board(int totalRows, int totalCols)
         {
             this.TotalRows = totalRows;
             this.TotalCols = totalCols;
 
-            this.Field = new char[this.TotalRows, this.TotalCols];
+            this.GenerateField();
         }
 
-        public char[,] Field { get; private set; }
+       // public char[,] Field { get; private set; }
         
         public int TotalCols { get; private set; }
         
-
         public int TotalRows { get; private set; }
+
+        private void GenerateField()
+        {
+            this.field = new char[this.TotalRows, this.TotalCols];
+
+            for (int r = 0; r < this.TotalRows; r++)
+            {
+                for (int c = 0; c < this.TotalCols; c++)
+                {
+                    this.field[r, c] = ' ';
+                }
+            }
+        }
     }
 }
