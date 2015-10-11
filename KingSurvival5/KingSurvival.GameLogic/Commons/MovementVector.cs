a58e0a672b9b65a058a26a893412a7a649e76c80
DennyGD <denny.dimitrova@abv.bg>
2015-10-11 12:49:13 +0300

    Introduced struct MovementVector

---
+++ KingSurvival5/KingSurvival.GameLogic/Commons/MovementVector.cs
@@ -0,0 +1,18 @@
+﻿namespace KingSurvival.GameLogic.Commons
+{
+    using KingSurvival.GameLogic.Contracts;
+
+    public struct MovementVector
+    {
+        public MovementVector(int rowMovement, int colMovement)
+            : this()
+        {
+            this.ByRow = rowMovement;
+            this.ByCol = colMovement;
+        }
+
+        public int ByRow { get; set; }
+
+        public int ByCol { get; set; }
+    }
+}
