2725491c0fc3f654f846061287e13c8527081377
DennyGD <denny.dimitrova@abv.bg>
2015-10-11 12:59:42 +0300

    Changed IMovementStrategy specs; Introduced PawnMovStrat

--- KingSurvival5/KingSurvival.GameLogic/MovementStrategies/BasicPawnMovementStrategy.cs
+++ KingSurvival5/KingSurvival.GameLogic/MovementStrategies/BasicPawnMovementStrategy.cs
@@ -2,17 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
-
     using KingSurvival.GameLogic.Commons;
 
-    public class BasicKingMovementStrategy : IMovementStrategy
+    public class BasicPawnMovementStrategy : IMovementStrategy
     {
         public IEnumerable<MovementVector> GetMovements()
         {
             var movements = new List<MovementVector>()
             {
-                new MovementVector(-1, -1),
-                new MovementVector(-1, 1),
                 new MovementVector(1, -1),
                 new MovementVector(1, 1),
             };
