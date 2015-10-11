e3a54968f52950b4789c95ff24cd9dc546a2b90c
DennyGD <denny.dimitrova@abv.bg>
2015-10-11 12:45:32 +0300

    Added properties validation

--- KingSurvival5/KingSurvival.GameLogic/Models/Position.cs
+++ KingSurvival5/KingSurvival.GameLogic/Models/Position.cs
@@ -1,9 +1,17 @@
 ﻿namespace KingSurvival.GameLogic.Models
 {
+    using System;
+
     using KingSurvival.GameLogic.Contracts;
 
     public class Position : IPosition
     {
+        private const string ExceptionMessage = "Position's coordinates must be positive values.";
+
+        private int row;
+
+        private int col;
+
         // Probably, there's no need of empty ctor. It is here for now in order to not break the code.
         public Position()
         {
@@ -15,8 +23,38 @@
             this.Col = col;
         }
 
-        public int Row { get; set; }
+        public int Row
+        {
+            get
+            {
+                return this.row;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessage);
+                }
 
-        public int Col { get; set; }
+                this.row = value;
+            }
+        }
+
+        public int Col
+        {
+            get
+            {
+                return this.col;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessage);
+                }
+
+                this.col = value;
+            }
+        }
     }
 }
