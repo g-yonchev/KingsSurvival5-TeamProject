a131a9fc0a1a98012673a40af904721556e8f2bf
g-yonchev <georgi.d.yonchev@gmail.com>
2015-10-12 14:32:39 +0300

    Tests for Factories

---
+++ KingSurvival5/KingSurvival.Tests/FactoriesTests/FactoriesTests.cs
@@ -0,0 +1,26 @@
+﻿namespace KingSurvival.Tests.FactoriesTests
+{
+    using KingSurvival.GameLogic.Models;
+    using KingSurvival.GameLogic.Models.Factories;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FactoriesTests
+    {
+        [TestMethod]
+        public void CreateFigureMethodTesterForKing()
+        {
+            var kingFactory = new KingFactory();
+            var king = kingFactory.CreateFigure('K', new Position(1, 1));
+            Assert.IsTrue(king is King);
+        }
+
+        [TestMethod]
+        public void CreateFigureMethodTesterForPawn()
+        {
+            var pawnFactory = new PawnFactory();
+            var king = pawnFactory.CreateFigure('A', new Position(2, 2));
+            Assert.IsTrue(king is Pawn);
+        }
+    }
+}
